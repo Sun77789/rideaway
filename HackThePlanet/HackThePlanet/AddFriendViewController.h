22e8270e07a83840e76778c32beea0599fef3179
Sun77789 <aubakirova.m.m@gmail.com>
2015-08-15 00:40:08 -0700

    AddFriendControlelr

---
+++ HackThePlanet/HackThePlanet/AddFriendViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddFriendViewController.h
+//  HackThePlanet
+//
+//  Created by Malika Aubakirova on 8/15/15.
+//  Copyright (c) 2015 MLH_Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddFriendViewController : UIViewController
+
+@end
