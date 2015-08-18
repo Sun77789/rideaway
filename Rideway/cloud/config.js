672832649cfd112e92b7261b8338ebfd120e5403
Joseph Milla <jjtmilla@gmail.com>
2015-08-18 11:25:19 -0700

    Add site to gh-pages

--- Rideway/cloud/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    "keys": {
-        "twilio": {
-            "AccountSID": "AC319268b3a15a67756b445cd0f24f06b3",
-            "AuthToken": "4824e403c73de5a58808d3d48e946453",
-            "number": "+15005550006"
-        }
-    }
-};
